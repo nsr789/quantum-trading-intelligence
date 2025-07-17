12b05972b31829b6472c49ecd8204cbd8b0c434b
NSR <109800395+nsr789@users.noreply.github.com>
2025-07-18 01:37:47 +0530

    Update constants.py

--- src/config/constants.py
+++ src/config/constants.py
@@ -9,3 +9,5 @@ SUPPORTED_INTERVALS = ["1d", "1wk", "1mo"]
 MAX_CANDLE_POINTS = 5_000  # plotly down-sample guard
 SENTIMENT_ROLLING_WINDOW = 7  # days
 BACKTEST_START_DELTA = timedelta(days=730)  # 2 years default look-back
+FORECAST_LOOKBACK = 120   # days
+FORECAST_HORIZON  = 5     # trading days
